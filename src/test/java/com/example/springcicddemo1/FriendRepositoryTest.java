3aceb369bdf5ed413292d80e8c856e7c43c47e6c
unico67 <unicodaum@hanmail.net>
2025-12-12 12:13:18 +0900

    네 번째 커밋

--- src/test/java/com/example/springcicddemo1/FriendRepositoryTest.java
+++ src/test/java/com/example/springcicddemo1/FriendRepositoryTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
@@ -29,10 +26,7 @@ public class FriendRepositoryTest {
 
     @Container
     static MySQLContainer<?> mysql =
-            new MySQLContainer<>("mysql:8.0")
-                    .withDatabaseName("edudb")
-                    .withUsername("jdbctest")
-                    .withPassword("jdbctest");
+            new MySQLContainer<>("mysql:8.0");
 
     @Test
     @Rollback(false)
